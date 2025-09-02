cfb0f73c8bb8a1c54e07c979ddd215ccea84e326
shubhi09 <sharma.shubhi0995@gmail.com>
2025-09-02 18:14:44 +0200

    react mounted for the opening screen

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="h-screen grid place-items-center bg-black">
+      <h1 className="text-4xl font-extrabold text-white">
+        Tailwind is working ✅
+      </h1>
+    </div>
+  );
+}
